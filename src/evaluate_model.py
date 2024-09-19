d3994c544fa61df9c4696a1434071d5fd6133fb6
shreelasapkota <sapkotashreela5@gmail.com>
2024-09-19 23:17:02 +0545

    to be updated

--- src/evaluate_model.py
+++ src/evaluate_model.py
@@ -1,5 +1,33 @@
 from sklearn.metrics import classification_report, confusion_matrix, accuracyscore
 import pickle
 
+#evaluate the model
+def evaluate_model(model, X_test, y_test):
+    y_pred = model.predict(X_test)
+    accuracy = accuracyscore(y_test, y_pred)
+    print(f"Accuracy: {accuracy}")
+
+    print("classification report:")
+    print(classification_report(y_test, y_pred))
+
+    print("Confusion matrix: ")
+    print(confusion_matrix(y_test, y_pred))
 
 
+#load and evaluate the model
+def load_and_evaluate_model(X_test, y_pred):
+    #load the model
+    with open('models/random_forest_classifier.pkl', 'rb') as s:
+        model = pickle.load(s) 
+
+    #evaluate the model
+    evaluate_model(model, X_test, y_pred)
+
+if __name__ == '__main__':
+    from feature_extraction import extract_features_and_labels, split_data
+    from preprocess import load_and_preprocess_csv_data
+
+    data = load_and_preprocess_csv_data('data/lyrics.csv')
+    X_tfidf, y, _ = extract_features_and_labels(data)
+    X_train, X_test, y_train, y_test = split_data(X_tfidf, y)
+    load_and_evaluate_model(X_test, y_test)
--- src/preprocess.py
+++ src/preprocess.py
@@ -1,21 +1,33 @@
 import pandas as pd 
 import nltk
 from nltk.corpus import stopwords
-import re
+from nltk.tokenize import word_tokenize
+import string
 
 nltk.download('stopwords') 
 
 
-#Cleaning the lyrics
-def clean_lyrics(text):
-    text=re.sub(r'd+','', text)  
-    text = re.sub(r'[^\w\s]', '', text)
-    text=text.lower()
-    text=' '.join([word for word in text.split() if word not in stopwords.words('english')])
-    return text
-
 #Load and preprocess the CSV file data
-def load_and_process_csv_data(csv_file):
+def load_and_preprocess_csv_data(csv_file):
     df = pd.read_csv(csv_file)
-    df['clean_lyrics'] = df['lyrics'].apply(clean_lyrics)
-    return df
+    lyrics = df['lyrics']
+    genres = df['type']
+    stop_words = stopwords.words('english')
+    def clean_lyrics(text):
+        if isinstance(text,str):
+            text = text.lower()
+            text = ''.join([char for char in text if char not in string.punctuation])
+            text = ' '.join([word for word in text if word not in stop_words ]) 
+            return text
+        else:
+            return '' #if the text is not string type
+        
+    df['cleaned_lyrics'] = lyrics.apply(clean_lyrics)
+    return df[['cleaned_lyrics', 'type']]
+
+if __name__ == '__main__':
+    csv_path = 'data/lyrics.csv'
+
+    preprocessed_data = load_and_preprocess_csv_data(csv_path)
+    print(preprocessed_data.head())
+
