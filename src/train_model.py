63383b996af92e9b61b9f185efe01145bd6535a8
shreelasapkota <sapkotashreela5@gmail.com>
2024-09-23 22:58:42 +0545

    Implementation of Random Forest model for genre classification

--- src/train_model.py
+++ src/train_model.py
@@ -6,28 +6,6 @@ from feature_extraction import extract_features_and_labels
 from preprocess import load_and_preprocess_csv_data
 import pickle
 
-# def train_random_forest(X, y):
-#     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-#  #random_test does the work of seed
-
-#     model = RandomForestClassifier(n_estimators = 100, random_state=42)
-
-#     model.fit(X_train, y_train)
-    
-#     return model, X_test, y_test
-
-# if __name__ == '__main__':
-
-   
-#     data = load_and_preprocess_csv_data('data/lyrics.csv')
-#     X_tfidf, y, _ = extract_features_and_labels(data)
-
-#     model, X_test, y_test = train_random_forest(X_tfidf, y)
-#     y_pred = model.predict(X_test)
-
-#     print(f"Accuracy: {accuracy_score(y_test, y_pred):.4f}")
-#     print("Classification Report:")
-#     print(classification_report(y_test, y_pred))
 
 def train_random_forest(X, y):
     model = RandomForestClassifier(random_state=42)
